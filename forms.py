9bb3af879ea95c3a5b4543d87e60c17f35677ff2
Slowlybomb <slyusar.gleb.ua@gmail.com>
2024-09-12 00:57:59 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,3 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, RadioField, SelectField, IntegerField, TextAreaField, FileField
 from wtforms.validators import InputRequired, EqualTo, DataRequired, NumberRange
+
+
